1f394bf8a6d8e8831d836443f8e645378c7028e6
Jan Gorol <jan.gorol@zentity.com>
2020-03-21 23:49:44 +0100

    Tap description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+// To play 4-7
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { CurrentChallengeComponent } from "./challenges/current-challenge/current-challenge.component";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -18,12 +19,12 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptFormsModule,
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        CurrentChallengeComponent,
     ],
     providers: [],
     schemas: [
