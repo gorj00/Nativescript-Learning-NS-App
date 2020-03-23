0a113251c4cb5265be597d2a4a7d0b5add134448
Jan Gorol <jan.gorol@zentity.com>
2020-03-23 19:37:41 +0100

    ListView virtualization

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-
+import { ItemEventData } from 'tns-core-modules/ui/list-view';
 @Component({
     selector: 'ns-current-challenge',
     templateUrl: './current-challenge.component.html',
@@ -8,4 +8,8 @@ import { Component, Input } from '@angular/core';
 
 export class CurrentChallengeComponent {
     @Input() challenges: string[] = [];
+
+    onItemTap(args: ItemEventData) {
+        console.log('Args', args);
+    }
 }
